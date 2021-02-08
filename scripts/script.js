31c5d9573ff34e5c8efaff45c6e9eba37d98ecb1
keertikaa <67870667+keertikaa@users.noreply.github.com>
2021-02-08 16:10:45 +0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ let formCount = 0;
 var close = false;
 
 async function myRequest(url) {
-  const host = "http://localhost:8080";
+  const host = "https://ades-2b03.herokuapp.com";
   try {
     return await axios.get(host + url);
   } catch (err) {
